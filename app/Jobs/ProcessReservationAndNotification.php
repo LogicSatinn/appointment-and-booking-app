e00a48f6cd286e984b648ea0b10cfaae44ba9e80
Calvin Jackson <amigohmccclurkin98@gmail.com>
2022-07-24 22:01:01 +0300

    minor fix

---
+++ app/Jobs/ProcessReservationAndNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Enums\ReservationStatus;
+use App\Models\Reservation;
+use App\Models\User;
+use App\Notifications\ClientReservationMadeNotification;
+use App\Notifications\NewReservationMadeNotification;
+use App\Services\BeemSmsService;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessReservationAndNotification implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $booking;
+    protected $client;
+    protected $timetable;
+    protected $clientTimetable;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($booking, $client, $timetable)
+    {
+        $this->booking = $booking;
+        $this->client = $client;
+        $this->timetable = $timetable;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws GuzzleException
+     */
+    public function handle()
+    {
+        $this->clientTimetable = $this->timetable->clients()->where('client_id', $this->client->id)->first();
+
+        for ($i = 1; $i <= $this->clientTimetable->pivot_no_of_seats; $i++) {
+            Reservation::create([
+                'client_id' => $this->client->id,
+                'timetable_id' => $this->timetable->id,
+                'booking_id' => $this->booking->id,
+                'seat_number' => rand(1, $this->timetable->resource->capacity),
+                'status' => ReservationStatus::BOOKED,
+                'reference_code' => 'NL-R' . rand(0000000, 9999999),
+                'reserved_at' => now()
+            ]);
+        }
+
+        $this->client->notify(new ClientReservationMadeNotification($this->clientTimetable, $this->booking));
+        User::find(1)->notify(new NewReservationMadeNotification($this->timetable, $this->clientTimetable));
+        (new BeemSmsService())->content('Hello There. Your reservation has been secured. You will receive an email with further details.')
+            ->getRecipients([$this->client->phone_number])
+            ->send();
+    }
+}
