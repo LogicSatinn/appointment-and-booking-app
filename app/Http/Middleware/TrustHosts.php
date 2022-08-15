9896cb51d073bfba3687d199dd1949fcce3c64fe
Calvin Jackson <amigohmccclurkin98@gmail.com>
2022-08-15 19:27:42 +0300

    minor fix

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -15,7 +15,6 @@ class TrustHosts extends Middleware
     {
         return [
             $this->allSubdomainsOfApplicationUrl(),
-            'http://nia-lab.calvinjackson.me'
         ];
     }
 }
